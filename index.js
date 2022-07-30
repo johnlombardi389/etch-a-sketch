65893b904641c7b96b4ca72b11e3f83a78f0beda
johnlombardi389 <lombardij38@gmail.com>
2022-07-30 15:40:06 -0400

    Combining functions to take input and build grid

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const container_div = document.getElementById('container');
 
+//Create blocks to place in grid
 function makeGrid(blocks) {
   let grid = blocks * 2;
 
   for(let i = 0; i < grid; i++) {
     let gridBlock = document.createElement('div');
     gridBlock.className = "defaultGridBlock";
-    document.getElementById('container').appendChild(gridBlock);
+    container_div.appendChild(gridBlock);
   }
 }
 
@@ -15,3 +16,20 @@ function () {
   container_div.style.gridTemplateColumns();
   container_div.style.gridTemplateRows();
 }*/
+/*
+function buildGrid(columns, rows) {
+  container_div.style.gridTemplateColumns(columns + "fr");
+  container_div.style.gridTemplateRows(rows);
+}*/
+
+function buildGrid(rows, cols) {
+  container_div.style.setProperty('--grid-rows', rows);
+  container_div.style.setProperty('--grid-cols', cols);
+  for (let i = 0; i < (rows * cols); i++) {
+    let gridBlock = document.createElement('div');
+    gridBlock.className = "defaultGridBlock";
+    container_div.appendChild(gridBlock);
+  }
+}
+
+buildGrid(16, 16);
