83214df0c27ea6351f6a0f5d1b42964e1ee47488
johnlombardi389 <lombardij38@gmail.com>
2022-08-01 14:58:18 -0400

    Added header and footer to html

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const container_div = document.getElementById('container');
-
+/*
 //Create blocks to place in grid
 function makeGrid(blocks) {
   let grid = blocks * 2;
@@ -9,24 +9,14 @@ function makeGrid(blocks) {
     gridBlock.className = "defaultGridBlock";
     container_div.appendChild(gridBlock);
   }
-}
-
-/*
-function () {
-  container_div.style.gridTemplateColumns();
-  container_div.style.gridTemplateRows();
-}*/
-/*
-function buildGrid(columns, rows) {
-  container_div.style.gridTemplateColumns(columns + "fr");
-  container_div.style.gridTemplateRows(rows);
 }*/
 
 function buildGrid(rows, cols) {
   container_div.style.setProperty('--grid-rows', rows);
   container_div.style.setProperty('--grid-cols', cols);
+
   for (let i = 0; i < (rows * cols); i++) {
-    let gridBlock = document.createElement('div');
+    const gridBlock = document.createElement('div');
     gridBlock.className = "defaultGridBlock";
     container_div.appendChild(gridBlock);
   }
