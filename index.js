30548850add17b10e73271e3bf85cf2611e571b9
johnlombardi389 <lombardij38@gmail.com>
2022-08-05 19:05:05 -0400

    Fixed buttons to set canvas grid size

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
+const DEFAULT_SIZE = 16
+
+let currentSize = DEFAULT_SIZE;
+
 const container_div = document.getElementById('container');
-/*
-//Create blocks to place in grid
-function makeGrid(blocks) {
-  let grid = blocks * 2;
+/* Buttons */
+const eight_btn = document.getElementById('eight');
+const sixteen_btn = document.getElementById('sixteen');
+const threetwo_btn = document.getElementById('threetwo');
+const sixfour_btn = document.getElementById('sixfour');
 
-  for(let i = 0; i < grid; i++) {
-    let gridBlock = document.createElement('div');
-    gridBlock.className = "defaultGridBlock";
-    container_div.appendChild(gridBlock);
-  }
-}*/
 
-function buildGrid(rows, cols) {
-  container_div.style.setProperty('--grid-rows', rows);
-  container_div.style.setProperty('--grid-cols', cols);
+function buildGrid(size) {
+  container_div.innerHTML = '';
+  container_div.classList.remove('defaultGridBlock');
+  container_div.style.setProperty('--grid-rows', size);
+  container_div.style.setProperty('--grid-cols', size);
 
-  for (let i = 0; i < (rows * cols); i++) {
+  for (let i = 0; i < (size * size); i++) {
     const gridBlock = document.createElement('div');
     gridBlock.className = "defaultGridBlock";
     container_div.appendChild(gridBlock);
   }
 }
 
-buildGrid(16, 16);
+buildGrid(currentSize);
+
+eight_btn.addEventListener('click', e => {
+  currentSize = 8;
+  buildGrid(currentSize);
+});
+
+sixteen_btn.addEventListener('click', e => {
+  currentSize = 16;
+  buildGrid(currentSize);
+});
+
+threetwo_btn.addEventListener('click', e => {
+  currentSize = 32;
+  buildGrid(currentSize);
+});
+
+sixfour_btn.addEventListener('click', e => {
+  currentSize = 64;
+  buildGrid(currentSize);
+});
+
